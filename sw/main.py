6a24f2bd53b30f7bc2bb23097d9242d9bb67bfb3
oksoti <mz542@cam.ac.uk>
2026-02-02 11:44:31 +0000

    config pin update

--- sw/main.py
+++ sw/main.py
@@ -1,13 +1,15 @@
 from utime import sleep_ms
+
+from sw.src.config import LEFT_PWM_PIN, RIGHT_PWM_PIN
 from sw.src.drivers.line_sensor import LineSensorArray
 from sw.src.drivers.motor import Motor, MotorPair
 from sw.src import config
 
 # --- Motor pin configuration (UPDATE to match your wiring) ---
-LEFT_MOTOR_DIR_PIN = 6
+'''LEFT_MOTOR_DIR_PIN = 6
 LEFT_MOTOR_PWM_PIN = 7
 RIGHT_MOTOR_DIR_PIN = 8
-RIGHT_MOTOR_PWM_PIN = 9
+RIGHT_MOTOR_PWM_PIN = 9'''
 
 # --- Speeds ---
 BASE_SPEED = 0.5
@@ -16,8 +18,8 @@ REALIGN_SPEED = 0.3
 # --- Hardware init ---
 line_sensor = LineSensorArray(config.LINE_PINS, white_is_1=config.LINE_WHITE_IS_1)
 
-left_motor = Motor(LEFT_MOTOR_DIR_PIN, LEFT_MOTOR_PWM_PIN)
-right_motor = Motor(RIGHT_MOTOR_DIR_PIN, RIGHT_MOTOR_PWM_PIN)
+left_motor = Motor(config.LEFT_DIR_PIN, LEFT_PWM_PIN)
+right_motor = Motor(config.RIGHT_DIR_PIN, RIGHT_PWM_PIN)
 motors = MotorPair(left_motor, right_motor)
 
 print("Running. Press Ctrl+C to stop.")
