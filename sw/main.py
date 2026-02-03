d9ab20d947f26eedce5b7830bc45e7d42ee47947
rachelwald1 <121107329+rachelwald1@users.noreply.github.com>
2026-02-03 09:38:06 +0000

    Refactor imports and update motor speeds

--- sw/main.py
+++ sw/main.py
@@ -1,9 +1,9 @@
 from utime import sleep_ms
 
-from sw.src.config import LEFT_PWM_PIN, RIGHT_PWM_PIN
-from sw.src.drivers.line_sensor import LineSensorArray
-from sw.src.drivers.motor import Motor, MotorPair
-from sw.src import config
+from config import LEFT_PWM_PIN, RIGHT_PWM_PIN
+from line_sensor import LineSensorArray
+from motor import Motor, MotorPair
+import config
 
 # --- Motor pin configuration (UPDATE to match your wiring) ---
 '''LEFT_MOTOR_DIR_PIN = 6
@@ -12,8 +12,8 @@ RIGHT_MOTOR_DIR_PIN = 8
 RIGHT_MOTOR_PWM_PIN = 9'''
 
 # --- Speeds ---
-BASE_SPEED = 0.3
-REALIGN_SPEED = 0.8
+BASE_SPEED = 0.8
+REALIGN_SPEED = 0.3
 
 # --- Hardware init ---
 line_sensor = LineSensorArray(config.LINE_PINS, white_is_1=config.LINE_WHITE_IS_1)
