6a06775511ad1d9dbc46f18cca421765b1c85208
rachelwald1 <121107329+rachelwald1@users.noreply.github.com>
2026-02-10 09:43:25 +0000

    servo architecture

--- sw/src/controllers/grabber.py
+++ sw/src/controllers/grabber.py
@@ -3,3 +3,19 @@ class Grabber:
     def __init__(self, servo_1, servo_2):
         self.servo_1 = servo_1
         self.servo_2 = servo_2
+
+    def open(self):
+        self.servo_1.set_angle(0)  # Open position for servo 1
+        self.servo_2.set_angle(0)  # Open position for servo 2
+
+    def close(self):
+        self.servo_1.set_angle(90)  # Closed position for servo 1
+        self.servo_2.set_angle(90)  # Closed position for servo 2
+
+    def toggle(self, is_open):
+        if is_open:
+            self.close()
+        else:
+            self.open()
+
+    
