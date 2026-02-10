582084bc5a79043b20a008d6f0f3dd014cfe5477
rachelwald1 <121107329+rachelwald1@users.noreply.github.com>
2026-02-10 10:19:28 +0000

    Refactor set_angle to handle current and desired angles

--- sw/src/drivers/servo.py
+++ sw/src/drivers/servo.py
@@ -8,20 +8,20 @@ class Servo:
         self.pwm.duty_u16(0)  # Start with the servo off
         sleep(0.1)  # Short delay to ensure the servo initializes
 
-    def set_angle(self, angle):
-        duty = int(1500 + (angle / 270) * 7000)  # 1500 for 0 degrees, 8500 for 270 degrees
+    def set_angle(self, current_angle, desired_angle):
+        duty = int(1500 + (desired_angle / 270) * 7000)  # 1500 for 0 degrees, 8500 for 270 degrees
         self.pwm.duty_u16(duty)
-        while True:
-            for duty in range(1500,8500,100):
-                self.pwm.duty_u16(duty)
-                #print(f"Raw Value: {raw_value}, Voltage: {voltage:.2f}V")
-                print(f"Processing position: {duty}")
-                sleep(0.02)
+        if (desired_angle > current_angle):  
+            for duty in range(current_angle,desired_angle,20):
                 
-            for duty in range(8500,1500,-100):
                 self.pwm.duty_u16(duty)
                 #print(f"Raw Value: {raw_value}, Voltage: {voltage:.2f}V")
                 print(f"Processing position: {duty}")
                 sleep(0.02)
 
-
+        if (desired_angle < current_angle):    
+            for duty in range(desired_angle,current_angle,-20):
+                self.pwm.duty_u16(duty)
+                #print(f"Raw Value: {raw_value}, Voltage: {voltage:.2f}V")
+                print(f"Processing position: {duty}")
+                sleep(0.02)
